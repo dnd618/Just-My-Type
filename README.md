47a656ba9f8c29e7bc3fe9e88b0c4e692686ef9b
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-23 15:04:25 -0500

    random push

--- README.md
+++ README.md
@@ -41,13 +41,13 @@
 
 ```
 
-var sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
+var sentences = ["ten ate neite ate nee enet ite ate inet ent eate",
 
-'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain
+"Too ato too nOt enot one totA not anot tOO aNot", "oat itain oat tain
 
-nate eate tea anne inant nean', 'itant eate anot eat nato inate eat
+nate eate tea anne inant nean", "itant eate anot eat nato inate eat
 
-anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+anot tain eat", "nee ene ate ite tent tiet ent ine ene ete ene ate"];
 
 ```
 
--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
 //Hiding keyboard but not showing when shift btn clicked
-$(document).ready(function(){
-    $(document).keydown(function(e){
-        if(e.keyCode == 15){
+$(document).ready(
+    function(){
+    $(document).keydown('keypress',function(e){
+        if(e.keyCode == 16){
             $('keyboard-upper-container').hide();
             $('keyboard-lower-container').show(); 
         }
+        });
     })
-    $(document).keyup(function(e){
-        if (e.keyCode == 14){
+    $(document).keyup(function(e)
+    {
+        if (e.keyCode == 16){
             $('keyboard-upper-container').hide();
             $('keyboard-lower-container').show(); 
         }
-     })
- });
+     });
+
