61bb4de28738075b7bd32a8014c334f5a6e87eb6
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-23 16:05:28 -0500

    added in-class example for txt hghlight function

--- typeit.js
+++ typeit.js
@@ -1,5 +1,33 @@
- //Text Animation Function
-// $(document).ready(function(){
+ //Text Animation & Highlight Function
+$(document).ready(function() {
+    var sentences 'ten ate neite ate nee enet ite ate inet ent eate',
+
+        'Too ato too nOt enot one totA not anot tOO aNot", "oat itain oat tain',
+
+        'nate eate tea anne inant nean", "itant eate anot eat nato inate eat',
+
+        'anot tain eat", "nee ene ate ite tent tiet ent ine ene ete ene ate'.
+        }
+$(document).on('keydown, function  (key) {
+    handleHighlight(key.which);
+
+});
+document.on ('keydown", function(key) {
+    handleHighlight(key.which, true);
+});
+function handleHighlight('code' defaultColor) {
+    if (defaultColor) {
+        $('#'+code).css(background-color, 'white');
+    } else {
+        $('#'=code).css('background-color, 'pink');
+    }
+}
+function initialize() {
+    sentenceContainer = $('sentences');
+    sentenceContainer.append(sentences[sentenceIndex]);
+}
+};
+};
 // $('.type-it').typeIt({
 //      speed: 900,
 //      lifeLike: false,
@@ -35,13 +63,7 @@
 // });
 //
 //
-// var sentences = ["ten ate neite ate nee enet ite ate inet ent eate",
 
-// "Too ato too nOt enot one totA not anot tOO aNot", "oat itain oat tain",
-
-// "nate eate tea anne inant nean", "itant eate anot eat nato inate eat",
-
-// "anot tain eat", "nee ene ate ite tent tiet ent ine ene ete ene ate"],
 
 // document.getElementById("myTypewriter").addEventListener("animationend", changeTxt);
 
