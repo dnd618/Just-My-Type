122235468d1f04294983cf3bca1fc18f47b1ed64
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-23 12:14:23 -0500

    added function for hide/show keyboard working

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
-    $('.togglebutton').click(function() {
-    $('.togglediv').toggleClass('hidden');
-    $('.togglebutton').toggleClass('nodisplay');
-    });
-});
+//Hiding keyboard but not showing when shift btn clicked
+$(document).ready(function(){
+    $(document).keydown(function(e){
+        if(e.keyCode == 17){
+            $('keyboard-upper-container').hide();
+            $('keyboard-lower-container').show(); 
+        }
+    })
+    $(document).keyup(function(e){
+        if (e.keyCode == 16){
+            $('keyboard-upper-container').hide();
+            $('keyboard-lower-container').show(); 
+        }
+     })
+ });
