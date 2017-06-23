7ff42b7a1b2fe6e4835561b126857f827ba482fb
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-23 17:11:07 -0500

    git commit -m "made style changes to css"

--- typeit.js
+++ typeit.js
@@ -1,80 +1,31 @@
  //Text Animation & Highlight Function
-$(document).ready(function() {
-    var sentences 'ten ate neite ate nee enet ite ate inet ent eate',
 
-        'Too ato too nOt enot one totA not anot tOO aNot", "oat itain oat tain',
+//Part of in-class example 
+// $(document).ready(function() {
+//     var sentences 'ten ate neite ate nee enet ite ate inet ent eate',
 
-        'nate eate tea anne inant nean", "itant eate anot eat nato inate eat',
+//         'Too ato too nOt enot one totA not anot tOO aNot", "oat itain oat tain',
 
-        'anot tain eat", "nee ene ate ite tent tiet ent ine ene ete ene ate'.
-        }
-$(document).on('keydown, function  (key) {
-    handleHighlight(key.which);
+//         'nate eate tea anne inant nean", "itant eate anot eat nato inate eat',
 
-});
-document.on ('keydown", function(key) {
-    handleHighlight(key.which, true);
-});
-function handleHighlight('code' defaultColor) {
-    if (defaultColor) {
-        $('#'+code).css(background-color, 'white');
-    } else {
-        $('#'=code).css('background-color, 'pink');
-    }
-}
-function initialize() {
-    sentenceContainer = $('sentences');
-    sentenceContainer.append(sentences[sentenceIndex]);
-}
-};
-};
-// $('.type-it').typeIt({
-//      speed: 900,
-//      lifeLike: false,
-//      autoStart: false
+//         'anot tain eat", "nee ene ate ite tent tiet ent ine ene ete ene ate'.
 // });
-// // .tiType('I am typing slowly,')
-// // .tiSettings({speed: 100})
-// // .tiType('but now I am typing pretty fasst')
-// // .tiDelete(2)
-// // .tiType('t!');
-
-//  tiType('A jQuery pl')
-// .tiPause(500)
-// .tiDelete(12)
-// .tiPause(500)
-// .tiType('The <strong>most versatile</strong> jquery')
-// .tiPause(200)
-// .tiDelete(5)
-// .tiType('Query typing plugin on the net.')
-// .tiPause(500)
-// .tiDelete(11)
-// .tiPause(350)
-// .tiType('on the ')
-// .tiPause(600)
-// .tiType('<em>planet.</em>');
-// // the queue I want to type
-//  $('#keyboard-upper-container', '#keyboard-lower-container').typeIt({
-//      // options
+// $(document).on('keydown, function  (key) {
+//     handleHighlight(key.which)
+//     });
+// document.on ('keydown", function(key) {
+//     handleHighlight(key.which, true);
 // });
-
-// $('.type-it').typeIt({
-//      strings: ['ten ate neite ate nee enet ite ate inet ent eate,']
-// });
-//
-//
-
-
-// document.getElementById("myTypewriter").addEventListener("animationend", changeTxt);
-
-// function changeTxt(e){
-//   _h1 = this.getElementsByTagName("h1")[0];
-//   _h1.style.webkitAnimation = 'none'; // set element animation to none
-//    setTimeout(function() { // you surely want a delay before the next message appears
-//       _h1.innerHTML=numberOfMistakes[rank];
-//       var speed =2 * numberOfMistakes[rank].length/20; // adjust the speed (3.5 is the original speed, 20 is the original string length
-//       _h1.style.webkitAnimation = 'typing '+speed+'s steps(40, end), blink-caret .75s step-end infinite'; //  switch to the original set of animation      
-//       (rank===numberOfMistakes.length-1)?rank=0:rank++; // if you have displayed the last message from the array, go back to the first one, else go to next message
-//     }, 1000);
+// function handleHighlight('code' defaultColor) {
+//     if (defaultColor) {
+//         $('#'+code).css(background-color, 'white');
+//     } else {
+//         $('#'=code).css('background-color, 'pink');
+//     }
 // }
-
+// function initialize() {
+//     sentenceContainer = $('sentences');
+//     sentenceContainer.append(sentences[sentenceIndex]);
+// }
+//     });
+// });
