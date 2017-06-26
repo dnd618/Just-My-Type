f33352739d6b94ab4009eb6492cc8792194abaa5
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-26 17:17:58 -0500

    added example for letter count

--- scripts.js
+++ scripts.js
@@ -38,11 +38,27 @@ var sents = ["ten ate neite ate nee enet ite ate inet ent eate",
 "oat itain oat tain nate eate tea anne inant nean", 
 "itant eate anot eat nato inate eat anot tain eat" , 
 "nee ene ate ite tent tiet ent ine ene ete ene ate"];
-console.log(sents[0]);
-console.log(sents);
-
-$("#sentence").append( sents[0]);
-// // //Highlight keys in-class example
+// console.log(sents[0]);
+// console.log(sents);
+//IN CLASS EXAMPLE FOR SENTENCES; Basically you need to keep a variable counter for any keys you wanna count
+sentenceCounter = 0;
+val currentLetterCode = currentSentence.charCodeAt(letterCounter);
+function handleSentences() {
+$('#sentence').empty();
+$('#sentence').append(sentences[sentenceCounter]);
+    sentenceCounter++;
+}
+letterCounter = 0;
+currentSentence = sentence[sentenceCounter];
+function handleResponse(e) {
+    console.log(e);
+    if (e.type === 'keyup') {
+    currentSentence = sentence[sentenceCounter];
+    }
+}
+// //
+// $('#sentence').append(sentences[sentenceCounter]);
+// // // //Highlight keys in-class example
 // // $(document).on('keydown, function(e) {
 // //     handleHighlight(e.which);
 // //     console.log(e.which)
@@ -80,26 +96,15 @@ $( document ).ready(function() {
         }
     });
     $(document).on('keydown', function(e) {
-        // if (e.which >= 65 || e.which <= 90 || e.which === 32 && e.which == e.shiftKey) {
-        //         console.log(e.charCode + 65);
-        // } 
         if(e.which === 48 || e.which >= 48 || e.which <= 57) {
             addHighlight(e.which);
         }
-    //     if (e.shiftKey && e.which === 49){
-    //         addHighlight(e.preventDefualt);
-    // }
         if(e.which >= 65 || e.which <= 90){
              addHighlight(e.which + 32);
         }
-        // if (e.whcih >= 51 || e.whcih >= 53 && e.shiftKey){
-        //     addHighlight(e.which - 18);
-        // if(e.shiftKey == false){
-        //     addHighlight(e.charCode);
         if (e.which === 49 && e.shiftKey){
             addHighlight(e.which -16);
         }
-        // }
      });    
     $(document).on('keyup', function(e) {
         if (e.which >= 65 || e.which <= 90 || e.which >= 123 || e.which <= 126 && e.shiftKey) {
@@ -111,36 +116,25 @@ $( document ).ready(function() {
         if(e.which >= 65 && e.which <= 90){
              removeHighlight(e.which + 32);
         }
-        //  if(e.which >= 65 || e.which <= 90){
-        //      removeHighlight(e.which + 32);
-        // } 
         if (e.which === 49 && e.shiftKey){
             removeHighlight(e.which -16);
         }
     }); 
      $(document).on('keypress', function(e) {
         if(e.which){
-            appendLetter(e.which);
+            $('#target-letter').string.fromCharCode.append(e.which);  
+
+            // appendLetter(e.which);
         }
      }); 
-    $('#sentence').append(str1);  
 
 });
 function addHighlight(code){
-        // if (defaultColor){
-        //     $('#' + code).css('background-color', '#f5f5f5');
-        // } else {
-            $('#' + code).css('background-color', 'yellow');
-        // }
+            $('#' + code).css('background-color', 'thistle');
 }
 function removeHighlight(code, defaultColor){
-        // if (defaultColor){
-        //     $('#' + code).css('background-color', '#f5f5f5');
-        // } else {
             $('#' + code).css('background-color', '#f5f5f5');
-        // }
 }
-
 function appendLetter (code){
     if(code == 116){
         $('#target-letter').append(String.fromCharCode(code));
