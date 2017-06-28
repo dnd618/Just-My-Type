3f0bbc5c383c03ce45f6fe42f446ad8d04575412
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-28 09:45:35 -0500

    rough final draft...very rough

--- typeit.js
+++ typeit.js
@@ -1,148 +1,84 @@
- //Text Animation & Highlight Function
+//Adding sentences
+var sentences =[
+    
+    'Ten ate neite ate nee enet ite ate inet ent eate',
+    'Too ato too nOt enot one totA not anot tOO aNot',
+    'oat itain oat tain nate eate tea anne inant nean',
+    'itant eate anot eat nato inate eat anot tain eat',
+    'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+var s = -1;
+var l = 0;
+var sentLength = sentences[l].length;
+var numTick = 0;
+var correct = $('<span />').attr({'class':'glyphicon glyphicon-ok', 'aria-hidden':'true' });
+var incorrect = $('<span />').attr({'class':'glyphicon glyphicon-remove', 'aria-hidden':'true' });
+var numWrong = 0;
+var numberOfWords = 54;
+var start = new Date();
+//Keyboard functions
+$(document).ready(function() {
+    $('#keyboard-upper-container').hide();
+    
+$(document).keyup(function() {
+    if(event.which == 16) {
+        $('#keyboard-lower-container').show();
+        $('#keyboard-upper-container').hide();
+    }   
+});
 
-//Part of in-class example 
-// $(document).ready(function() {
-//     var sentences 'ten ate neite ate nee enet ite ate inet ent eate',
+$(document).keydown(function() {
+    if(event.which == 16) {
+        $('#keyboard-lower-container').hide();
+        $('#keyboard-upper-container').show();
+    }   
+});
 
-//         'Too ato too nOt enot one totA not anot tOO aNot", "oat itain oat tain',
+$('body').keypress(function(e) {
+    keyPress = String.fromCharCode(event.which);
+});
+//Key highlights
+$(document).keypress(function(event) {
+    $('#' + event.which).css('background-color', 'thistle');
+        $(document).keyup(function() {
+            $('#' + event.which).css('background-color', '#f5f5f5');
+        });
+});
 
-//         'nate eate tea anne inant nean", "itant eate anot eat nato inate eat',
+    $('#sentence').html((sentences[0].split('')));
 
-//         'anot tain eat", "nee ene ate ite tent tiet ent ine ene ete ene ate'.
-// });
-// $(document).on('keydown, function  (key) {
-//     handleHighlight(key.which)
-//     });
-// document.on ('keydown", function(key) {
-//     handleHighlight(key.which, true);
-// });
-// function handleHighlight('code' defaultColor) {
-//     if (defaultColor) {
-//         $('#'+code).css(background-color, 'white');
-//     } else {
-//         $('#'=code).css('background-color, 'pink');
-//     }
-// }
-// function initialize() {
-//     sentenceContainer = $('sentences');
-//     sentenceContainer.append(sentences[sentenceIndex]);
-// }
-//     });
-// });
-//IN CLASS EXAMPLE FOR SENTENCES; Basically you need to keep a variable counter for any keys you wanna count
-sentenceCounter = 0;
-var currentLetterCode = currentSentence.charCodeAt(letterCounter);
-function handleSentences() {
-$('#sentence').empty();
-$('#sentence').append(sentences[sentenceCounter]);
-    sentenceCounter++;
-}
-letterCounter = 0;
-currentSentence = sentence[sentenceCounter];
-function handleResponse(e) {
-    console.log(e);
-    if (e.type === 'keyup') {
-    currentSentence = sentence[sentenceCounter];
+    $('body').on('keypress', function() {
+        s++;
+        push();
+    });
+});
+$(document).on('keypress', function(e) {
+        if(e.which){
+            $('#target-letter').append(String.fromCharCode(e.which));  
+            //Purple box animation
+            $("#purple-block").animate({ "left" : "1000px" }, 3500);    
+            }
+        }); 
+//Trying to calculate WPM
+function push() {
+    numTick++;
+    var sentChar = sentences[l].charAt(s);
+    $('#target-letter').text(sentChar);
+    if (sentChar == keyPress) {
+        $('#feedback').html(correct);
+    } 
+    else if (numTick == 50){
+        gameEnd();
+    } else if (numTick >= sentences[l].length) {
+        l++;
+        $('#yellow-block').css('margin-left', "-15px");
+        $('#sentence').html((sentences[l]).split(''));
+        $('#feedback').html('');
+        numTick = 0;
+        s = -1;
+        return;
+    } 
+    else {
+        $('#feedback').html(incorrect);
+        numWrong++;
     }
-}
-var sentenceCounter = 0;
-sentenceCounter = 0;
-var currentLetterCode = currentSentence.charCodeAt(letterCounter);
-function handleSentences() {
-    var sentenceDIV = $('#sentence').empty();
-    $('#sentence').append(sentences[sentenceCounter]);
-        sentenceCounter++;
-    }
-// letterCounter = 0;
-// currentSentence = sentence[sentenceCounter];
-// function handleResponse(e) {
-//     console.log(e);
-//     if (e.type === 'keyup') {
-//     currentSentence = sentence[sentenceCounter];
-//     }
-// }
-// function handleSentences () {
-// $("#sentence").html( sents[i]);
-// i++
-
-// }
-// // //Highlight keys in-class example
-// // $(document).on('keydown, function(e) {
-// //     handleHighlight(e.which);
-// //     console.log(e.which)
-// // document.on ('keydown", function(key) {
-// //     handleHighlight(key.which, true);
-// // });
-// // function handleHighlight(code, kill) {
-// //     if (!kill) {
-// //         $('#' + code).css('background-color', 'white');
-// //     } else (!kill) {
-// //         $('#' + code).css('background-color', '#lightcoral');
-// //     }
-// // }    
-// //Trying to get keyboard highlights
-// // function handleHighlight(code, defaultColor){
-// //         if (defaultColor){
-// //             $('#' + code).css('background-color', '#f5f5f5');
-// //         } else {
-// //             $('#' + code).css('background-color', '#thistle');
-// //         }//
-// //Simple function to hide/show keyboard 
-// $(document).ready(function(){
-//     var upper = $('#keyboard-upper-container'),
-//         lower = $('#keyboard-lower-container');
-
-//     upper.hide();
-//     $(document).on('keyup keydown', function(e) {
-//         if (e.shiftKey) {
-//             lower.hide();
-//             upper.show();
-//         } else {
-//             upper.hide();
-//             lower.show();
-//         }
-//         if (e.type === 'keydown') {
-//             handleHighlight(e, 'thistle');
-//         } else {
-//             handleHighlight(e, '#f5f5f5');
-//         }
-//     });
-// });
-//Creating reusable function for highlight
-// function handleHighlight(e, color) {
-//     var code = e.keyCode;
-//     if (!e.isShift && utils.isAlpha(code)) {
-//         code = e.keyCode +32;
-//     }
-//     if (utils.isAlphanumeric(code)) {
-//         $('#'+code).css('background-color', color);
-//     } else {
-//         $('span:contains(' +e.key+ ')').css('background-color', color);
-//     }
-// }
-
-//CharCodeAt example; function where the computer knows what characters to look at
-// var hello = 'hello';
-// undefined
-// var letterCounter = 0;
-// undefined
-// function test() {
-//     console.log(hello.charCodeAt(letterCounter);
-//     letterCounter++;
-// }
-// VM2894:2 Uncaught SyntaxError: missing ) after argument list
-// function test() {
-//     console.log(hello.charCodeAt(letterCounter));
-//     letterCounter++;
-// }
-// undefined
-// test();
-// test();
-// test();
-// test();
-// test();
-// console output :2 104
-// console output :2 101
-// console output :2 108
-// console output :2 108
-// console output :2 111
+};
