df51fb1e7e6528d877e15d66196d83773e0b468c
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-08-15 09:14:04 -0500

    final project

--- scripts.js
+++ scripts.js
@@ -1,84 +1,180 @@
-//Adding sentences
-var sentences =[
-    
-    'Ten ate neite ate nee enet ite ate inet ent eate',
-    'Too ato too nOt enot one totA not anot tOO aNot',
-    'oat itain oat tain nate eate tea anne inant nean',
-    'itant eate anot eat nato inate eat anot tain eat',
-    'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-var s = -1;
-var l = 0;
-var sentLength = sentences[l].length;
-var numTick = 0;
-var correct = $('<span />').attr({'class':'glyphicon glyphicon-ok', 'aria-hidden':'true' });
-var incorrect = $('<span />').attr({'class':'glyphicon glyphicon-remove', 'aria-hidden':'true' });
-var numWrong = 0;
-var numberOfWords = 54;
-var start = new Date();
-//Keyboard functions
+var upper,
+    lower,
+    sentences = [
+        'ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'
+    ],
+    currentSentence = '',
+    sentenceCounter = 0,
+    letterCounter = 0,
+    time,
+    mistakes = 0;
+
 $(document).ready(function() {
-    $('#keyboard-upper-container').hide();
-    
-$(document).keyup(function() {
-    if(event.which == 16) {
-        $('#keyboard-lower-container').show();
-        $('#keyboard-upper-container').hide();
-    }   
-});
+    upper = $('#keyboard-upper-container');
+    lower = $('#keyboard-lower-container');
 
-$(document).keydown(function() {
-    if(event.which == 16) {
-        $('#keyboard-lower-container').hide();
-        $('#keyboard-upper-container').show();
-    }   
-});
+    upper.hide();
+    setSentence();
+    handleTargets(letterCounter);
 
-$('body').keypress(function(e) {
-    keyPress = String.fromCharCode(event.which);
-});
-//Key highlights
-$(document).keypress(function(event) {
-    $('#' + event.which).css('background-color', 'thistle');
-        $(document).keyup(function() {
-            $('#' + event.which).css('background-color', '#f5f5f5');
-        });
-});
+    $(document).on('keyup keydown', function(e) {
+        var trueCode = configKeyCode(e);
 
-    $('#sentence').html((sentences[0].split('')));
+        if (time === undefined) {
+            time = new Date().getTime();
+        }
 
-    $('body').on('keypress', function() {
-        s++;
-        push();
+        if (e.keyCode === 16) {
+            handleShift(e);
+        }
+
+        handleHighlight(e, trueCode);
+
+        if (e.type === 'keyup') {
+            handleResponse(trueCode);
+        }
     });
 });
-$(document).on('keypress', function(e) {
-        if(e.which){
-            $('#target-letter').append(String.fromCharCode(e.which));  
-            //Purple box animation
-            $("#purple-block").animate({ "left" : "1000px" }, 3500);    
-            }
-        }); 
-//Trying to calculate WPM
-function push() {
-    numTick++;
-    var sentChar = sentences[l].charAt(s);
-    $('#target-letter').text(sentChar);
-    if (sentChar == keyPress) {
-        $('#feedback').html(correct);
-    } 
-    else if (numTick == 50){
-        gameEnd();
-    } else if (numTick >= sentences[l].length) {
-        l++;
-        $('#yellow-block').css('margin-left', "-15px");
-        $('#sentence').html((sentences[l]).split(''));
-        $('#feedback').html('');
-        numTick = 0;
-        s = -1;
+
+function handleResponse(code) {
+    if (code === currentSentence.charCodeAt(letterCounter)) {
+        setGlyphicon(true);
+        letterCounter++;
+
+        if (letterCounter >= currentSentence.length) {
+            letterCounter = 0;
+            setSentence();
+        }
+
+        handleTargets(letterCounter);
+    } else {
+        mistakes++;
+        setGlyphicon(false);
+    }
+}
+
+function setGlyphicon(isCorrect) {
+    $('#feedback').empty();
+
+    if (isCorrect) {
+        $('#feedback').append('<span class="glyphicon glyphicon-ok"></span>');
+    } else {
+        $('#feedback').append('<span class="glyphicon glyphicon-remove"></span>');
+    }
+}
+
+function setSentence() {
+    if (sentenceCounter >= sentences.length) {
+        gameOver();
         return;
-    } 
-    else {
-        $('#feedback').html(incorrect);
-        numWrong++;
+    }
+
+    currentSentence = sentences[sentenceCounter];
+
+    var sentenceContainer = $('#sentence'),
+        splitSentence = currentSentence.split(''),
+        constructedSentence = '';
+
+    for (var i = 0; i < splitSentence.length; i++) {
+        constructedSentence += `<span id='letter-${i}'>${currentSentence[i]}</span>`
+    }
+
+    sentenceContainer.empty();
+    sentenceContainer.append(constructedSentence);
+    sentenceCounter++;
+}
+
+function handleTargets(counter) {
+    var targetDiv = $('#target-letter');
+    targetDiv.empty();
+
+    if (counter > 0) {
+        $('#letter-' + (counter-1)).css('background-color', 'lavenderblush');
+    }
+
+    targetDiv.append(currentSentence[letterCounter]);
+    $('#letter-' + counter).css('background-color', 'lavenderblush');
+}
+
+function configKeyCode(e) {
+    if (utils.isAlpha(e.keyCode) && !e.shiftKey) {
+        return e.keyCode + 32;
+    } else {
+        return e.keyCode;
+    }
+}
+
+function handleShift(event) {
+    if (event.type === 'keydown') {
+        toggleKeyboard('upper');
+    } else if (event.type === 'keyup') {
+        toggleKeyboard('lower');
+    }
+}
+
+function handleHighlight(event, code) {
+    if (event.type === 'keydown') {
+        $('#' + code).css('background-color', 'lavenderblush');
+    } else if (event.type === 'keyup') {
+        $('#' + code).css('background-color', 'lavenderblush');
+    }
+}
+
+function toggleKeyboard(type) {
+    if (type === 'upper') {
+        lower.hide();
+        upper.show();
+    } else if (type === 'lower') {
+        upper.hide();
+        lower.show();
+    }
+}
+
+function gameOver() {
+    handleWPM();
+
+    var playAgain = confirm(
+        `your wpm score is: ${handleWPM()}
+        do you want to play, again?`
+    );
+
+    if (playAgain) {
+        location.reload(true);
+    }
+}
+
+function handleWPM() {
+    var elapsed = new Date().getTime() - time;
+    var numberOfWords = sentences.reduce(function(acc, curr) {
+        return acc += curr.split(' ').length;
+    }, 0);
+
+    var minutes = elapsed / 60000;
+
+    return Math.round(numberOfWords / minutes - 2 * mistakes);
+}
+
+var utils = {
+    isAlphanumeric: function(code) {
+        if ((code >= 65 && code <= 90) || (code >= 48 && code <= 57)) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    isNumber: function(code) {
+        if (code >= 48 && code <= 57) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    isAlpha: function(code) {
+        if (code >= 65 && code <= 90) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
+
